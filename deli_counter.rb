d550aa4cad5b299f03b2c95c5283da8ac9bfd9e4
molly mk <mmkover@gmail.com>
2020-08-21 19:41:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli) 
+  if deli.empty? 
+    puts "The line is currently empty."
+  else 
+    line_string = "The line is currently:"
+    deli.map.with_index(1) do |name, i|
+      line_string << " #{i}. #{name}"
+    end
+    puts line_string
+  end
+end
+
+def take_a_number(deli, name)
+  if deli.empty?
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    deli << "name"
+  end
+  deli
+end
